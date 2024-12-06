6231168e660bfe16ef03e241abb209c2ee3898bb
Momen <abdulmomen.official@gmail.com>
2024-12-06 11:19:00 +0600

    template updated

--- templete.py
+++ templete.py
@@ -1,55 +1,36 @@
-import os 
+import os
 from pathlib import Path
 import logging
 
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
 
-project_name = "hackathon"
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+   " test.py"
+]
 
-# list_of_files = [
-#     f"{project_name}/components/__init__.py",
-#     f"{project_name}/components/data_ingestion.py",
-#     f"{project_name}/components/data_transformation.py",
-#     f"{project_name}/components/model_trainer.py",
-#     f"{project_name}/components/model_evaluation.py",
-#     f"{project_name}/components/model_pusher.py",
-#     f"{project_name}/configuration/__init__.py",
-#     f"{project_name}/configuration/gcloud_syncer.py",
-#     f"{project_name}/constants/__init__.py",
-#     f"{project_name}/entity/__init__.py",
-#     f"{project_name}/entity/config_entity.py",
-#     f"{project_name}/entity/artifact_entity.py",
-#     f"{project_name}/exception/__init__.py",
-#     f"{project_name}/logger/__init__.py",
-#     f"{project_name}/pipeline/__init__.py",
-#     f"{project_name}/pipeline/train_pipeline.py",
-#     f"{project_name}/pipeline/prediction_pipeline.py",
-#     f"{project_name}/ml/__init__.py",
-#     f"{project_name}/ml/model.py",
-#     "app.py",
-#     "demo.py",
-#     "requirements.txt",
-#     "Dockerfile",
-#     "setup.py",
-#     ".dockerignore"  # Fixed missing comma here
-]   
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-
-    # Separate file directory and filename
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
-    
-    # Check if file doesn't exist or is empty
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(f"{filename} already exists")
-    
+        logging.info(f"{filename} is already exists")
